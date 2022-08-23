bb5dd4649b46c10bb3d141d9569f5ec4dc2e9555
Gabriel Oliveira <gabrielgamersbr@gmail.com>
2022-08-23 19:54:29 -0300

    new components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,3 +1,6 @@
+import { HighLightCard } from "../../components/HighLightCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard/index";
+
 import React from "react";
 import {Container, 
         Header,
@@ -8,15 +11,61 @@ import {Container,
         UserGreeting,
         UserName,
         Icon,
-        HighLightCards
+        HighLightCards,
+        Transactions,
+        Title,
+        TransactionList,
 } from "./styles"
-import { RFValue } from "react-native-responsive-fontsize";
-import { HighLightCard } from "../../components/HighLightCard";
+
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+
+}
 
 export default function Dashboard () {
+  
+    const data: DataListProps[] = [{
+    id: '1',
+    type: 'positive',
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+    },
+    date: "13/04/2020"
+},
+
+{
+
+    id: '2',
+    type: 'negative',
+    title: "Hamburgueria Dog",
+    amount: "R$ 59,00",
+    category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+        },
+        
+    date: "10/04/2020"
+},
+
+{
+    id: '3',
+    type: 'negative',
+    title: "Aluguel do apartamento",
+    amount: "R$ 1200,00",
+    category: {
+        name: 'Casa',
+        icon: 'shopping-bag'
+    },
+    date: "10/04/2020"
+}
+
+];
 
     return(
-    
+
     <Container>
 
         <Header>
@@ -56,10 +105,22 @@ export default function Dashboard () {
            
         </HighLightCards>
 
+        <Transactions>
+            <Title>Listagem</Title>
+        
+        <TransactionList
+        data={data}
+        keyExtractor={ item => item.id }
+        renderItem={({ item }) => <TransactionCard data={item}/>}
+        />
+
+        </Transactions>
     </Container>
 
-
-    )
-
+   )
 
 }
+// function getBottomSpace() {
+//     throw new Error("Function not implemented.");
+// }
+
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,7 +2,8 @@ import styled from "styled-components/native"
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import theme from "../../global/styles/theme";
 import { Feather } from "@expo/vector-icons";
-
+import { DataListProps } from ".";
+import { FlatList, FlatListProps } from "react-native";
 
 export const Container = styled.View`
     flex: 1;
@@ -81,3 +82,22 @@ export const HighLightCards = styled.ScrollView.attrs({
     position: absolute;
     margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions =  styled.View`
+    flex: 1%;
+    padding: 0 24px;
+    font-family: ${({theme}) => theme.fonts.regular};
+    margin-top: ${RFPercentage(12)}px;
+
+`;
+export const Title =  styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular };
+    margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+    FlatList as new (props: FlatListProps<DataListProps>) => FlatList<DataListProps>    
+    ).attrs({
+
+})``;
